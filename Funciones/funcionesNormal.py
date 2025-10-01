b299b3df0aa63aa07b3e0be49211ecb7888d0e4d
Luis_Duarte <luise.duarte29@gmail.com>
2025-10-01 14:57:18 -0300

    Funciones

---
+++ Funciones/funcionesNormal.py
@@ -0,0 +1,4 @@
+def hola(nombre="Defecto"): # Definición de la función Hola que recibe un parámetro nombre con valor por defecto "Defecto"
+    print(f"Hola Mundo{nombre}")
+
+hola("Pepe") # Llamada a la función Hola con el argumento "Pepe"
